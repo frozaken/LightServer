638694e0e3531bb198afec0966324b0eaaa64e12
frozaken <marcumail@gmail.com>
2018-10-26 14:54:24 +0200

    ECDHE

--- miniserver.py
+++ miniserver.py
@@ -2,10 +2,14 @@ import socket
 import threading
 import time
 import struct
-from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS1_v1_5
 import logging
-from cryptography.hazmat.primitives import ec
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.primitives import hashes
+from Crypto import Random
+from Crypto.Cipher import AES
 
 class Server:
     def __init__(self,host, port, concurrenctcons):
@@ -26,19 +30,9 @@ class Server:
 
         self.Logger = logger
 
-        self.InitCrypto()
-
         serv.listen(concurrenctcons)
 
         self.Socket = serv
-
-
-    def InitCrypto(self):
-        self.Logger.info("Generating RSA key...")
-        key = RSA.generate(4096)
-
-        self.Key = key
-        self.Decryptor = PKCS1_v1_5.new(key)
 
     def Start(self):
         self.Logger.info("Listening for connections...")
@@ -53,12 +47,47 @@ class Server:
 
         self.Logger.info("connected client on: %s"%(str(addr)))
 
-        pubkey = self.Key.publickey().exportKey('DER')
+        priv = ec.generate_private_key(
+            ec.SECP384R1(),
+            default_backend()
+        )
+
+        pubkey = priv.public_key().public_bytes(
+            encoding = serialization.Encoding.DER,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
 
         self.Logger.info("sending key %s... to %s"%(pubkey[:10],str(addr)))
 
         socket.send(pubkey)
 
+
+        #now recieve client key
+
+        clientpubkeybytes = socket.recv(120)
+
+        clientpubkey = serialization.load_der_public_key(
+            clientpubkeybytes,
+            default_backend()
+        )
+
+
+        sharedkey = priv.exchange(
+            ec.ECDH(),
+            clientpubkey
+        )
+
+        derived = HKDF(
+            algorithm = hashes.SHA256(),
+            length=32,
+            salt = None,
+            info = b'handshake data',
+            backend = default_backend()
+        ).derive(
+            sharedkey
+        )
+
+        self.Logger.info("derived key: %s..."%(derived[:10]))
 
         while True:
             block = socket.recv(512)
@@ -66,19 +95,30 @@ class Server:
                 break
             blocks += block
 
-        if len(blocks) != 512:
-            socket.close()
-            self.Logger.error("Incorrect recieve format, closing socket for %s"%(str(addr)))
-            return
-        dec = self.Decryptor.decrypt(blocks, b'\xdeadbeef')
+
+        IV = blocks[:AES.block_size]
+
+        aes = AES.new(
+            derived,
+            AES.MODE_CBC,
+            IV
+        )
 
 
-        if(len(dec) != 7):
+        dec_dat = aes.decrypt(blocks[AES.block_size:])
+
+        padding = int(dec_dat[-1])
+
+        final = dec_dat[:-padding]
+
+        fmt = "!3si"
+
+        if(len(final) != struct.calcsize(fmt)):
             socket.close()
             self.Logger.error("Incorrect struct length, closing socket for %s"%(str(addr)))
             return
 
-        tp = struct.unpack("!3si",bytearray(dec))
+        tp = struct.unpack(fmt ,bytearray(final))
 
         self.Logger.info("Client %s sent %s"%(str(addr), tp))
 
--- testclient.py
+++ testclient.py
@@ -1,9 +1,13 @@
 import socket
 import struct 
 import array
-from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS1_v1_5
-
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.primitives import hashes
+from Crypto import Random
+from Crypto.Cipher import AES
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -15,18 +19,58 @@ fmt  = "!3si"
 buf = bytearray(struct.calcsize(fmt))
 
 struct.pack_into(fmt,buf,0,'cus'.encode('UTF-8'),2)
-pubkey = sock.recv(550)
 
-print("Recieved key")
+privkey = peer_public_key = ec.generate_private_key(
+    ec.SECP384R1(), 
+    default_backend()
+)
 
-pubk = RSA.import_key(pubkey)
+servpubkeybytes = sock.recv(120)
 
-enctor = PKCS1_v1_5.new(pubk)
+sock.send(
+    privkey.public_key().public_bytes(
+        serialization.Encoding.DER,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+)
+
+servpubkey = serialization.load_der_public_key(
+    servpubkeybytes,
+    default_backend()
+)
 
 
-encd = enctor.encrypt(buf)
+sharedkey = privkey.exchange(
+    ec.ECDH(),
+    servpubkey
+)
 
-print(pubkey[:10])
+derived = HKDF(
+    algorithm = hashes.SHA256(),
+    length=32,
+    salt = None,
+    info = b'handshake data',
+    backend = default_backend()
+).derive(
+    sharedkey
+)
+
+IV = Random.new().read(AES.block_size)
+
+aes = AES.new(
+    derived,
+    AES.MODE_CBC,
+    IV
+)
+
+padding = AES.block_size - len(buf) % AES.block_size
+
+buf += bytes([padding]) * padding
+
+print("Derived key: %s"%derived[:10])
+encd = IV + aes.encrypt(buf)
+
+print(IV)
 
 print("Sending %s bytes: %s..."%(len(encd), encd[:10]))
 
